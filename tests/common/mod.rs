2efef0fdbf1ce497cbd0bffe7d68755f9745e258
giusbianco <white951@hotmail.it>
2023-12-11 09:51:02 +0100

    Update lib matches

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -3,18 +3,18 @@ use std::{fs, path::Path};
 use walkdir::WalkDir;
 
 pub fn compare_template(snapshot_path: &Path, template_path: &Path) {
-    for entry in WalkDir::new(template_path)
-        .into_iter() {
-            entry.map_or( (), |e| if e.path().is_file() {
+    for entry in WalkDir::new(template_path).into_iter() {
+        entry.map_or((), |e| {
+            if e.path().is_file() {
                 compare(snapshot_path, template_path, e.path());
-            })
-        }
+            }
+        })
+    }
 }
-
 
 fn compare(snapshot_path: &Path, path: &Path, entry: &Path) {
     let content = fs::read_to_string(entry).unwrap();
-    let name = entry.file_name().and_then(|v| v.to_str()); 
+    let name = entry.file_name().and_then(|v| v.to_str());
 
     insta::with_settings!({
         snapshot_path => snapshot_path
