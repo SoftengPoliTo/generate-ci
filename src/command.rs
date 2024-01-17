7f2fec8016b7ab397f49df8d2413655715367fee
giusbianco <white951@hotmail.it>
2024-01-17 17:08:59 +0100

    Update command.rs

--- src/command.rs
+++ src/command.rs
@@ -12,8 +12,8 @@ pub(crate) fn run_command(path: &Path, args: &[&str]) -> Result<ExitStatus> {
     let output = Command::new("cargo").args(args).arg(path).output()?;
 
     debug!(
-        "{:#?}",
-        str::from_utf8(&output.stdout).unwrap_or("Error converting bytes into a characters.")
+        "{}",
+        str::from_utf8(&output.stdout).unwrap_or("Error converting bytes into characters.")
     );
 
     Ok(output.status)
