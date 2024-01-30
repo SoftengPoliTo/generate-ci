348f88d6fbf21ce7b54c5d7a9281820f8f5ab506
Luni-4 <luni-4@hotmail.it>
2024-01-30 11:16:56 +0100

    cargo: Add xtask checks

--- src/toolchain/cargo.rs
+++ src/toolchain/cargo.rs
@@ -16,7 +16,26 @@ static CARGO_TEMPLATES: &[(&str, &str)] = &builtin_templates!["cargo" =>
     ("ci.github.docker", "github-docker-application.yml"),
     ("docker.amd64", "Dockerfile-amd64"),
     ("docker.arm64", "Dockerfile-arm64"),
-    ("rs.proptest", "proptest.rs")
+    ("rs.proptest", "proptest.rs"),
+    // Execute checks and tests locally through xtask
+    ("xtask.config", "xtask-config"),
+    ("xtask.toml", "xtask/Cargo.toml"),
+    // Tasks
+    ("xtask.src.books", "xtask/src/books.rs"),
+    ("xtask.src.dependencies", "xtask/src/dependencies.rs"),
+    ("xtask.src.logging", "xtask/src/logging.rs"),
+    ("xtask.src.main", "xtask/src/main.rs"),
+    ("xtask.src.publish", "xtask/src/publish.rs"),
+    ("xtask.src.runchecks", "xtask/src/runchecks.rs"),
+    ("xtask.src.vulnerabilities", "xtask/src/vulnerabilities.rs"),
+    // Utilities
+    ("xtask.src.utils.cargo", "xtask/src/utils/cargo.rs"),
+    ("xtask.src.utils.mdbook", "xtask/src/utils/mdbook.rs"),
+    ("xtask.src.utils.mod", "xtask/src/utils/mod.rs"),
+    ("xtask.src.utils.process", "xtask/src/utils/process.rs"),
+    ("xtask.src.utils.rustup", "xtask/src/utils/rustup.rs"),
+    ("xtask.src.utils.time", "xtask/src/utils/time.rs"),
+    ("xtask.src.utils.workspace", "xtask/src/utils/workspace.rs")
 ];
 
 /// A cargo project data.
@@ -84,6 +103,10 @@ impl<'a> Cargo<'a> {
         let root = project_path.to_path_buf();
         let github = project_path.join(".github/workflows");
         let docker = project_path.join("docker");
+        let cargo = project_path.join(".cargo");
+        let xtask = project_path.join("xtask");
+        let xtask_src = xtask.join("src");
+        let xtask_utils = xtask_src.join("utils");
 
         let mut template_files = HashMap::new();
 
@@ -102,13 +125,56 @@ impl<'a> Cargo<'a> {
         template_files.insert(docker.join("Dockerfile-amd64"), "docker.amd64");
         template_files.insert(docker.join("Dockerfile-arm64"), "docker.arm64");
 
+        // xtask configuration file for cargo
+        template_files.insert(cargo.join("config"), "xtask.config");
+
+        // xtask
+        template_files.insert(xtask.join("Cargo.toml"), "xtask.toml");
+        // xtask/src
+        template_files.insert(xtask_src.join("books.rs"), "xtask.src.books");
+        template_files.insert(xtask_src.join("dependencies.rs"), "xtask.src.dependencies");
+        template_files.insert(xtask_src.join("logging.rs"), "xtask.src.logging");
+        template_files.insert(xtask_src.join("main.rs"), "xtask.src.main");
+        template_files.insert(xtask_src.join("publish.rs"), "xtask.src.publish");
+        template_files.insert(xtask_src.join("runchecks.rs"), "xtask.src.runchecks");
+        template_files.insert(
+            xtask_src.join("vulnerabilities.rs"),
+            "xtask.src.vulnerabilities",
+        );
+        // xtask/src/utils
+        template_files.insert(xtask_utils.join("cargo.rs"), "xtask.src.utils.cargo");
+        template_files.insert(xtask_utils.join("mdbook.rs"), "xtask.src.utils.mdbook");
+        template_files.insert(xtask_utils.join("mod.rs"), "xtask.src.utils.mod");
+        template_files.insert(xtask_utils.join("process.rs"), "xtask.src.utils.process");
+        template_files.insert(xtask_utils.join("rustup.rs"), "xtask.src.utils.rustup");
+        template_files.insert(xtask_utils.join("time.rs"), "xtask.src.utils.time");
+        template_files.insert(
+            xtask_utils.join("workspace.rs"),
+            "xtask.src.utils.workspace",
+        );
+
         if !ci {
             // Proptest
             let tests = project_path.join("tests");
             template_files.insert(tests.join("proptest.rs"), "rs.proptest");
-            (template_files, vec![root, github, docker, tests])
+            (
+                template_files,
+                vec![
+                    root,
+                    github,
+                    docker,
+                    cargo,
+                    xtask,
+                    xtask_src,
+                    xtask_utils,
+                    tests,
+                ],
+            )
         } else {
-            (template_files, vec![root, github, docker])
+            (
+                template_files,
+                vec![root, github, docker, cargo, xtask, xtask_src, xtask_utils],
+            )
         }
     }
 }
